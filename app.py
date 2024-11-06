e800156c0843a965ab59562e773b8031e160771b
TYrueouJeL <guerinremi.pro@gmail.com>
2024-11-06 16:07:43 +0100

    Initialisation du projet

--- app.py
+++ app.py
@@ -23,3 +23,14 @@ def check_db_connection():
         return True
     except Exception as e:
         print("Failed to connect to database:", str(e))
+        return False
+
+@app.route('/')
+def index():
+    if check_db_connection():
+        return 'Database Connection Status: Connected'
+    else:
+        return 'Database Connection Status: Disconnected'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
